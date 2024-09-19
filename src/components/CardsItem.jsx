db76ef7aa4592d27a071450f896c98316e1deecd
Nikoline01 <146435826+Nikoline01@users.noreply.github.com>
2024-09-19 21:02:49 +0200

    send

--- src/components/CardsItem.jsx
+++ src/components/CardsItem.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import HOF from "/src/assets/HOF.png";
+import IS from "/src/assets/IS.png";
+import NIPPON from "/src/assets/Nippon.png";
+import MONK from "/src/assets/MONK.png";
 
 export default function CardsItem() {
   return (
@@ -9,15 +12,15 @@ export default function CardsItem() {
         <p>HOLM OUTDOOR FITNESS</p>
       </Link>
       <Link to="/Project" className="cards-item-link">
-        <img src="/src/assets/IS.png" alt="STORYTELLING"></img>
+        <img src={IS} alt="Interactive Storytelling" />
         <p>INTERACTIVE STORYTELLING</p>
       </Link>
       <Link to="/Project" className="cards-item-link">
-        <img src="/src/assets/NIPPON.png" alt="NIPPON"></img>
+        <img src={NIPPON} alt="NIPPON" />
         <p>NIPPON</p>
       </Link>
       <Link to="/Project" className="cards-item-link">
-        <img src="/src/assets/MONK.png" alt="MONK"></img>
+        <img src={MONK} alt="MONK" />
         <p>MONK</p>
       </Link>
     </li>
