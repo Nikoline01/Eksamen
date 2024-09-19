7dab805951d1b252f04adb9bd224d0608652281f
Nikoline01 <146435826+Nikoline01@users.noreply.github.com>
2024-09-19 19:56:44 +0200

    send

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/portfolio/"}>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Eksamen/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
